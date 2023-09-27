40e2114aade7ae21c1f1b42172bab2a30a17a5bd
jose <jose.silva2000@etec.sp.gov.br>
2023-09-27 20:11:26 -0300

    Colocamos js nas partes que foi necessario

---
+++ cadastro.js
@@ -0,0 +1,10 @@
+ 
+
+function test(){
+    if (document.getElementById("RM").textContent == "CPF")
+    {
+        document.getElementById("RM").textContent = "RM";
+    }
+    else {document.getElementById("RM").textContent = "CPF";
+    }
+}
